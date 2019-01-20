13e5aba75f737f04eaf99d3372aa5a82fa474cee
“Bean” <bean5389@gmail.com>
2019-01-20 20:33:36 +0900

    Initial Commit

---
+++ FindersSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindersSample
+//
+//  Created by 박 형빈 on 20/01/2019.
+//  Copyright © 2019 bean5389. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindersSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindersSample
+//
+//  Created by 박 형빈 on 20/01/2019.
+//  Copyright © 2019 bean5389. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
